e1899b3941941485eb12e6fdd51537452b03fdd8
Dmitry Grigorev <gridmi007@gmail.com>
2019-10-26 10:27:32 +0300

    images

--- router.py
+++ router.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import send_file, request, Flask
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/images/<string:name>.jpg', methods=['GET'])
+def get_image(name):
+    filename = './pictures/' + name + '.jpg'
+    return send_file(filename, mimetype='image/jpg')
 
 
 if __name__ == '__main__':
